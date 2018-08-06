e8f88ab321c9f704aac3fe27437a196fc1cc2a66
Mohsen Hasani <mohsen2hasani@gmail.com>
2018-08-06 16:38:01 +0430

    log error on payrequest

--- Adin.BankPayment/Controllers/PayInfoController.cs
+++ Adin.BankPayment/Controllers/PayInfoController.cs
@@ -39,108 +39,131 @@ namespace Adin.BankPayment.Controllers
         [HttpPost]
         public async Task<IActionResult> RequestPay([FromBody]PayInfoModel model)
         {
-            Application application = await _applicationRepository.GetFirstBy(x => x.PublicKey == model.PublicKey);
-            if (application == null || application.Status != 0)
+            try
             {
-                return Unauthorized();
+
+                Application application = await _applicationRepository.GetFirstBy(x => x.PublicKey == model.PublicKey);
+                if (application == null || application.Status != 0)
+                {
+                    return Unauthorized();
+                }
+                if (model.PriceUnit == PriceUnitEnum.Toman)
+                {
+                    model.Amount = model.Amount * 10;
+                }
+                if (model.PriceUnit == PriceUnitEnum.Dollar)
+                {
+                    throw new NotImplementedException("Dollar not supported yet");
+                }
+
+                //Todo: Find Best Bank For User
+                Bank bank = await _bankRepository.GetFirstBy(x => x.Code == (byte)model.BankCode);
+                var transaction = new Transaction()
+                {
+                    Amount = model.Amount,
+                    ApplicationId = application.Id,
+                    BankId = bank.Id,
+                    CreatedBy = 1,
+                    Mobile = model.Mobile,
+                    UserTrackCode = model.TrackCode,
+                    CreationDate = DateTime.Now,
+                    Status = (byte)TransactionStatusEnum.Initial,
+                    CallbackUrl = model.CallbackUrl,
+                    ExpirationTime = model.ExpirationTime
+                };
+                await _transactionRepository.Add(transaction);
+
+                var applicationBank = await _applicationBankRepository.GetFirstBy(x => x.ApplicationId == application.Id && x.BankId == bank.Id);
+
+                var currentBaseUrl = string.Format("{0}://{1}", Request.Scheme, Request.Host);
+
+                switch (model.BankCode)
+                {
+                    case BankCodeEnum.Parsian:
+                        var parsianRedirectUrl = string.Format("{0}/Parsian/Callback?token={1}&SecondTrackCode={2}", currentBaseUrl, model.TrackCode, transaction.Id);
+                        transaction.BankRedirectUrl = parsianRedirectUrl;
+                        await _transactionRepository.Update(transaction);
+                        return Ok(
+                            new PayRequestResponseModel
+                            {
+                                Code = transaction.Id,
+                                Url = currentBaseUrl + "/Pay/" + transaction.Id
+                            });
+
+                    case BankCodeEnum.Mellat:
+                        var mellatRedirectUrl = string.Format("{0}/Mellat/Callback?token={1}&SecondTrackCode={2}", currentBaseUrl, model.TrackCode, transaction.Id);
+                        transaction.BankRedirectUrl = mellatRedirectUrl;
+                        await _transactionRepository.Update(transaction);
+                        return Ok(
+                            new PayRequestResponseModel
+                            {
+                                Code = transaction.Id,
+                                Url = currentBaseUrl + "/Pay/" + transaction.Id
+                            });
+                    case BankCodeEnum.Saman:
+                    default:
+                        var redirectUrl = string.Format("{0}/Saman/Callback?token={1}&SecondTrackCode={2}", currentBaseUrl, model.TrackCode, transaction.Id);
+                        transaction.BankRedirectUrl = redirectUrl;
+                        await _transactionRepository.Update(transaction);
+                        return Ok(
+                            new PayRequestResponseModel
+                            {
+                                Code = transaction.Id,
+                                Url = currentBaseUrl + "/Pay/" + transaction.Id
+                            });
+                }
             }
-            if (model.PriceUnit == PriceUnitEnum.Toman)
+            catch (Exception ex)
             {
-                model.Amount = model.Amount * 10;
+                _logger.LogError(ex.Message);
+                if (ex.InnerException != null)
+                    _logger.LogError(ex.InnerException.Message);
+                throw;
             }
-            if (model.PriceUnit == PriceUnitEnum.Dollar)
-            {
-                throw new NotImplementedException("Dollar not supported yet");
-            }
 
-            //Todo: Find Best Bank For User
-            Bank bank = await _bankRepository.GetFirstBy(x => x.Code == (byte)model.BankCode);
-            var transaction = new Transaction()
-            {
-                Amount = model.Amount,
-                ApplicationId = application.Id,
-                BankId = bank.Id,
-                CreatedBy = 1,
-                Mobile = model.Mobile,
-                UserTrackCode = model.TrackCode,
-                CreationDate = DateTime.Now,
-                Status = (byte)TransactionStatusEnum.Initial,
-                CallbackUrl = model.CallbackUrl,
-                ExpirationTime=model.ExpirationTime
-            };
-            await _transactionRepository.Add(transaction);
-
-            var applicationBank = await _applicationBankRepository.GetFirstBy(x => x.ApplicationId == application.Id && x.BankId == bank.Id);
-
-            var currentBaseUrl = string.Format("{0}://{1}", Request.Scheme, Request.Host);
-
-            switch (model.BankCode)
-            {
-                case BankCodeEnum.Parsian:                
-                    var parsianRedirectUrl = string.Format("{0}/Parsian/Callback?token={1}&SecondTrackCode={2}", currentBaseUrl, model.TrackCode, transaction.Id);
-                    transaction.BankRedirectUrl = parsianRedirectUrl;
-                    await _transactionRepository.Update(transaction);
-                    return Ok(
-                        new PayRequestResponseModel
-                        {
-                            Code = transaction.Id,
-                            Url = currentBaseUrl + "/Pay/" + transaction.Id
-                        });
-
-                case BankCodeEnum.Mellat:
-                    var mellatRedirectUrl = string.Format("{0}/Mellat/Callback?token={1}&SecondTrackCode={2}", currentBaseUrl, model.TrackCode, transaction.Id);
-                    transaction.BankRedirectUrl = mellatRedirectUrl;
-                    await _transactionRepository.Update(transaction);
-                    return Ok(
-                        new PayRequestResponseModel
-                        {
-                            Code = transaction.Id,
-                            Url = currentBaseUrl + "/Pay/" + transaction.Id
-                        });
-                case BankCodeEnum.Saman:
-                default:
-                    var redirectUrl = string.Format("{0}/Saman/Callback?token={1}&SecondTrackCode={2}", currentBaseUrl, model.TrackCode, transaction.Id);
-                    transaction.BankRedirectUrl = redirectUrl;
-                    await _transactionRepository.Update(transaction);
-                    return Ok(
-                        new PayRequestResponseModel
-                        {
-                            Code = transaction.Id,
-                            Url = currentBaseUrl + "/Pay/" + transaction.Id
-                        });
-            }
         }
 
 
         [HttpGet()]
         public async Task<IActionResult> Verify(string publicKey, Guid id)
         {
-            Application application = await _applicationRepository.GetFirstBy(x => x.PublicKey == publicKey);
-            if (application == null || application.Status != 0)
+            try
             {
-                return Unauthorized();
+                Application application = await _applicationRepository.GetFirstBy(x => x.PublicKey == publicKey);
+                if (application == null || application.Status != 0)
+                {
+                    return Unauthorized();
+                }
+
+                var transaction = await _transactionRepository.Get(id);
+                if (transaction == null)
+                {
+                    return Unauthorized();
+                }
+
+                switch (transaction.Bank.Code)
+                {
+                    case ((byte)BankCodeEnum.Mellat):
+                        MellatHelper mellatHelper = new MellatHelper(_logger, _transactionRepository, _applicationRepository, _bankRepository, _applicationBankRepository);
+                        VerifyTransactionResponseModel mellatResult = await mellatHelper.VerifyTransaction(transaction);
+                        return Ok(mellatResult);
+
+                    case ((byte)BankCodeEnum.Saman):
+                    default:
+                        SamanHelper samanHelper = new SamanHelper(_logger, _transactionRepository, _applicationRepository, _bankRepository, _applicationBankRepository);
+                        VerifyTransactionResponseModel result = await samanHelper.VerifyTransaction(transaction);
+                        return Ok(result);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                if (ex.InnerException != null)
+                    _logger.LogError(ex.InnerException.Message);
+                throw;
             }
 
-            var transaction = await _transactionRepository.Get(id);
-            if (transaction == null)
-            {
-                return Unauthorized();
-            }
-
-            switch (transaction.Bank.Code)
-            {
-                case ((byte)BankCodeEnum.Mellat):                
-                    MellatHelper mellatHelper = new MellatHelper(_logger, _transactionRepository, _applicationRepository, _bankRepository, _applicationBankRepository);
-                    VerifyTransactionResponseModel mellatResult = await mellatHelper.VerifyTransaction(transaction);
-                    return Ok(mellatResult);
-
-                case ((byte)BankCodeEnum.Saman):
-                default:
-                    SamanHelper samanHelper = new SamanHelper(_logger, _transactionRepository, _applicationRepository, _bankRepository, _applicationBankRepository);
-                    VerifyTransactionResponseModel result = await samanHelper.VerifyTransaction(transaction);
-                    return Ok(result);                  
-
-            }
         }
 
 
