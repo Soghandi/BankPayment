70e8b65cd399334fc10f8698787ef1cd2062f3eb
sgohandi <mostafa.soghandi@gmail.com>
2018-09-09 14:52:00 +0430

    cancel file added

---
+++ Server/Adin.BankPayment.Connector/Model/CancelPaymentResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adin.BankPayment.Connector.Model
+{
+    public class CancelPaymentResponseModel
+    {
+        public bool Status { get; set; }
+
+        public byte ErrorCode { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
